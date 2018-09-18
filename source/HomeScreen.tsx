3e3106ea703f24b50edcf12e14a5e8f4efce7086
Thomas Hagström <thomas@crossplatform.se>
2018-09-18 14:44:49 +0200

    Documentation

--- source/HomeScreen.tsx
+++ source/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styles from './styles';
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { FormLabel, Icon } from 'react-native-elements';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
@@ -8,15 +8,21 @@ export const HomeScreen = () => (
     <View style={styles.container}>
         <Text style={styles.welcome}>React Native "native" SanityCheck</Text>
         <Text style={styles.instructions}>Tested Libraries:</Text>
+        <ScrollView>
         <View>
-            <FormLabel>- react-native-vector-icons</FormLabel>
-            <FormLabel>- react-native-elements</FormLabel>
-            <FormLabel>- react-apollo</FormLabel>
-            <FormLabel>- graphql</FormLabel>
-            <FormLabel>- apollo-boost</FormLabel>
-            <FormLabel>- aws-amplify</FormLabel>
-            <FormLabel>- react-navigation</FormLabel>
+            <FormLabel>- 16.5.0 react</FormLabel>
+            <FormLabel>- 0.57.0 react-native</FormLabel>
+            <FormLabel>- 3.0.3 typescript</FormLabel>
+            <FormLabel>- 23.1.4 ts-jest</FormLabel>
+            <FormLabel>- 2.14.2 react-navigation</FormLabel>
+            <FormLabel>- 5.0.0 react-native-vector-icons</FormLabel>
+            <FormLabel>- 0.19.0 react-native-elements</FormLabel>
+            <FormLabel>- 2.1.11 react-apollo</FormLabel>
+            <FormLabel>- 14.0.2 graphql</FormLabel>
+            <FormLabel>- 0.1.16 apollo-boost</FormLabel>
+            <FormLabel>- 1.1.1 aws-amplify</FormLabel>
         </View>
+        </ScrollView>
         <FormLabel>React-Native Elements with icons</FormLabel>
         <FontAwesome name='map-o' color='blue' size={48} />
         <Icon type='font-awesome' color='purple' name='user-o' size={48} />
