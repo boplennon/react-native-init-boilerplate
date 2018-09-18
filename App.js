276b00fcef9c8d3c9d18ecdb1382ab0fecf49bcc
Thomas Hagström <thomas@crossplatform.se>
2018-09-18 13:47:17 +0200

    Apollo Graphql added

--- App.js
+++ App.js
@@ -6,10 +6,12 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { FormLabel, Icon } from 'react-native-elements';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,17 +20,34 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'https://api.github.com/graphql'
+  },
+  )
+});
+
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <FormLabel>React-Native Elements with icons</FormLabel>
-        <FontAwesome name='map-o' color='blue' size={48} />
-        <Icon type='font-awesome' color='purple' name='user-o' size={48} />
-      </View>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>React Native "native" SanityCheck</Text>
+          <Text style={styles.instructions}>Libraries:</Text>
+          <View>
+            <FormLabel>- react-native-vector-icons</FormLabel>
+            <FormLabel>- react-native-elements</FormLabel>
+            <FormLabel>- react-apollo</FormLabel>
+            <FormLabel>- graphql</FormLabel>
+            <FormLabel>- apollo-boost</FormLabel>
+          </View>
+          <FormLabel>React-Native Elements with icons</FormLabel>
+          <FontAwesome name='map-o' color='blue' size={48} />
+          <Icon type='font-awesome' color='purple' name='user-o' size={48} />
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+      </ApolloProvider>
     );
   }
 }
@@ -39,6 +58,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    padding: 10,
+    margin: 5
   },
   welcome: {
     fontSize: 20,
