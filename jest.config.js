f93b97372120aa4fb9a2987fc19dbbf8eecadf51
Thomas Hagström <thomas@crossplatform.se>
2018-09-21 08:59:11 +0200

    Jest code coverage

--- jest.config.js
+++ jest.config.js
@@ -33,5 +33,11 @@ module.exports = {
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/config/fileMock.js"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ]
 };
